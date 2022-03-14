43ed27897c284db03ea46216b067c4bde2c41ea3
Frank Moreno <frankmoreno1993@gmail.com>
2022-03-13 22:08:11 -0500

    refactor: programs is a part of utils/index.dart

---
+++ lib/src/utils/programs.dart
@@ -0,0 +1,14 @@
+part of 'index.dart';
+
+// from chia/wallet/puzzles/p2_delegated_puzzle_or_hidden_puzzle.clvm
+final standardTransactionPuzzle = Program.deserializeHexFile(
+  'lib/src/standard/puzzles/p2_delegated_puzzle_or_hidden_puzzle.clvm.hex',
+);
+
+// from chia/wallet/puzzles/p2_delegated_puzzle_or_hidden_puzzle.py
+final defaultHiddenPuzzle = Program.parse('(=)');
+
+// from chia/wallet/puzzles/calculate_synthetic_public_key.clvm
+final calculateSyntheticKeyProgram = Program.deserializeHexFile(
+  'lib/src/core/puzzles/calculate_synthetic_public_key/calculate_synthetic_public_key.clvm.hex',
+);
