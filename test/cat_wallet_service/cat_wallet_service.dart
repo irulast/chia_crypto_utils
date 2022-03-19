1919072dc3b7fff41235f1c97fbdf90741f3c971
nvjoshi2 <nvj1300@gmail.com>
2022-03-18 19:13:30 -0500

    clean up cat test

--- test/cat_wallet_service/cat_wallet_service.dart
+++ test/cat_wallet_service/cat_wallet_service.dart
@@ -5,7 +5,7 @@ import 'package:chia_utils/src/cat/transport/transport.dart';
 import 'package:chia_utils/src/core/models/payment.dart';
 import 'package:test/test.dart';
 
-Future<void> main(List<String> args) async {
+Future<void> main() async {
   final configurationProvider = ConfigurationProvider()
     ..setConfig(NetworkFactory.configId, {
       'yaml_file_path': 'lib/src/networks/chia/testnet10/config.yaml'
@@ -29,8 +29,6 @@ Future<void> main(List<String> args) async {
       'lizard', 'vacuum', 'embody', 'track', 'crash', 'truth',
       'arrow', 'tree', 'poet', 'audit', 'grid', 'mesh',
   ];
-  // final testMnemonic = 'guilt rail green junior loud track cupboard citizen begin play west adapt myself panda eye finger nuclear someone update light dance exotic expect layer'.split(' ');
-  
 
   final masterKeyPair = MasterKeyPair.fromMnemonic(testMnemonic);
 
@@ -47,38 +45,31 @@ Future<void> main(List<String> args) async {
     .map((e) => e.assetIdtoOuterPuzzlehash[targetAssetId]!).toList();
   final catCoins = await catTransport.getCatCoinsByOuterPuzzleHashes(outerPuzzleHashesToSearchFor, targetAssetId);
   final standardCoins = await fullNode.getCoinRecordsByPuzzleHashes(walletKeychain.unhardenedMap.values.map((e) => e.puzzlehash).toList());
-  print('standard coin balance: ${standardCoins.fold(0, (int previousValue, element) => previousValue + element.amount)}');
-  print('cat coin balance: ${catCoins.fold(0, (int previousValue, element) => previousValue + element.amount)}');
-  // prev  standard: 10000001100
-  // after standard: 
 
-  // prev  cat: 3850
-
-  // after cat: 
-
-  print('n cat coins: ${catCoins.length}');
-  print(catCoins.map((e) => e.amount));
-
-  // final targetPuzzlehash = walletKeychain.unhardenedMap.values.toList()[1].puzzlehash;
+  final changePuzzlehash = walletKeychain.unhardenedMap.values.toList()[0].puzzlehash;
+  final targetPuzzlehash = walletKeychain.unhardenedMap.values.toList()[1].puzzlehash;
   //7
   // final targetPuzzlehash = Address('txch1ftnx4dxr05mestyguwcys3t23dn767nyd6py8w69l70hxmk7332s5f5zx6').toPuzzlehash();
   //3 
-  final targetPuzzlehash = Address('txch1dwd0y9czedfhmh420st0gnscyzv650yn2nnqlmgc5zavhnx2af8sfl20ra').toPuzzlehash();
+  // final targetPuzzlehash = Address('txch1dwd0y9czedfhmh420st0gnscyzv650yn2nnqlmgc5zavhnx2af8sfl20ra').toPuzzlehash();
   
-  final changePuzzlehash = walletKeychain.unhardenedMap.values.toList()[0].puzzlehash;
   
-  // test('Produces valid spendbundle', () async {
-  //   final payment = Payment(100, targetPuzzlehash);
-  //   final spendBundle = catWalletService.createSpendBundle([payment], [catCoins[0]], changePuzzlehash, walletKeychain);
-  //   spendBundle.debug();
-  //   // await fullNode.pushTransaction(spendBundle);
-  // });
+  test('Produces valid spendbundle', () async {
+    final payment = Payment(200, targetPuzzlehash);
+    final spendBundle = catWalletService.createSpendBundle([payment], [catCoins[0]], changePuzzlehash, walletKeychain);
+    await fullNode.pushTransaction(spendBundle);
+  });
 
   test('Produces valid spendbundle with fee', () async {
     final payment = Payment(200, targetPuzzlehash);
+    final spendBundle = catWalletService.createSpendBundle([payment], catCoins.sublist(1), changePuzzlehash, walletKeychain, fee: 1000, standardCoinsForFee: [standardCoins.firstWhere((element) => element.amount >= 1000)]);
+    await fullNode.pushTransaction(spendBundle);
+  });
+
+  test('Produces valid spendbundle with fee and multiple payments', () async {
+    final payment = Payment(200, targetPuzzlehash);
     final payment1 = Payment(100, targetPuzzlehash);
     final spendBundle = catWalletService.createSpendBundle([payment, payment1], catCoins.sublist(1), changePuzzlehash, walletKeychain, fee: 1000, standardCoinsForFee: [standardCoins.firstWhere((element) => element.amount >= 1000)]);
-    // spendBundle.debug();
     await fullNode.pushTransaction(spendBundle);
   });
 }
