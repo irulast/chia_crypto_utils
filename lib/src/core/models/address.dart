26af115606b780878b60c80a2d636a1e3856b07e
Frank Moreno <frankmoreno1993@gmail.com>
2022-03-22 12:22:31 -0500

    Address is immutbale

--- lib/src/core/models/address.dart
+++ lib/src/core/models/address.dart
@@ -1,12 +1,12 @@
 import 'package:bech32m/bech32m.dart';
 import 'package:chia_utils/src/core/models/puzzlehash.dart';
+import 'package:meta/meta.dart';
 
+@immutable
 class Address {
-  String address;
+  const Address(this.address);
 
-  Address(
-    this.address,
-  );
+  final String address;
 
   factory Address.fromPuzzlehash(Puzzlehash puzzlehash, String addressPrefix) {
     return Address(segwit.encode(Segwit(addressPrefix, puzzlehash.bytes)));
@@ -14,5 +14,18 @@ class Address {
 
   Puzzlehash toPuzzlehash() {
     return Puzzlehash(segwit.decode(address).program);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode ^ address.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    return other is Address && other.address == address;
+  }
+
+  @override
+  String toString() {
+    return 'Address($address)';
   }
 }
