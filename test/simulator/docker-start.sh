d82e819a516bd43744fca0ff600430f743c7f854
Adam J. Weigold <adam@adamweigold.com>
2022-03-24 00:55:24 -0500

    Update docker-start.sh
    Adding exit code on trap, removing directory listing on client config operation

--- test/simulator/docker-start.sh
+++ test/simulator/docker-start.sh
@@ -2,8 +2,8 @@
 set -eo
 
 if [[ -n "${CLIENT_CONFIG_DIR+x}" ]]; then
+  echo "Copying config directory for client tooling ..."
   cp -R /root/.chia/mainnet/config/. "${CLIENT_CONFIG_DIR}"
-  ls -al "${CLIENT_CONFIG_DIR}"
   chown -R $TARGET_UID:$TARGET_GID "${CLIENT_CONFIG_DIR}"
 fi
 
@@ -13,13 +13,12 @@ function get_chia_pid() {
 
 python chia/simulator/start_simulator.py
 
-trap "echo Shutting down ...; kill -9 $get_chia_pid; exit 0" SIGINT SIGTERM
+trap "echo Shutting down ...; kill -9 $get_chia_pid; exit 1" SIGINT SIGTERM
 
 while [ 1 ]
 do
   pid=$get_chia_pid
-  if [ "$pid"="" ]
-  then
+  if [ "$pid"="" ]; then
     exit
   fi
   sleep 3
