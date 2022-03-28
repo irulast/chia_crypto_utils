55c3d4f30026cdc29655ed57b9d694ab6e33d630
Marvin Quevedo <marvinquevedo2017@outlook.es>
2022-03-28 09:12:40 -0600

    root derive child for hardened and unhardened

--- lib/src/core/models/wallet_set.dart
+++ lib/src/core/models/wallet_set.dart
@@ -50,6 +50,46 @@ class WalletSet {
       derivationIndex: derivationIndex,
     );
   }
+
+  factory WalletSet.fromPrivateKeyWithRoot(
+      //  PrivateKey masterPrivateKey,
+      {
+    required PrivateKey rootChildPrivateKey,
+    required PrivateKey rootChildPrivateKeyUnhardened,
+    required int derivationIndex,
+  }) {
+    final childPrivateKeyHardened =
+        rootWalletSkToWalletSk(rootChildPrivateKey, derivationIndex);
+    final childPublicKeyHardened = childPrivateKeyHardened.getG1();
+
+    final puzzleHardened = getPuzzleFromPk(childPublicKeyHardened);
+    final puzzlehashHardened = Puzzlehash(puzzleHardened.hash());
+
+    final hardened = WalletVector(
+      childPrivateKey: childPrivateKeyHardened,
+      childPublicKey: childPublicKeyHardened,
+      puzzlehash: puzzlehashHardened,
+    );
+
+    final childPrivateKeyUnhardened = rootWalletSkToWalletSkUnhardened(
+        rootChildPrivateKeyUnhardened, derivationIndex);
+    final childPublicKeyUnhardened = childPrivateKeyUnhardened.getG1();
+
+    final puzzleUnhardened = getPuzzleFromPk(childPublicKeyUnhardened);
+    final puzzlehashUnhardened = Puzzlehash(puzzleUnhardened.hash());
+
+    final unhardened = UnhardenedWalletVector(
+      childPrivateKey: childPrivateKeyUnhardened,
+      childPublicKey: childPublicKeyUnhardened,
+      puzzlehash: puzzlehashUnhardened,
+    );
+
+    return WalletSet(
+      hardened: hardened,
+      unhardened: unhardened,
+      derivationIndex: derivationIndex,
+    );
+  }
 }
 
 @immutable
