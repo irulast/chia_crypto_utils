bd7587363746c845fd9f25060fdcd0ebbe3c05d1
nvjoshi2 <nvj1300@gmail.com>
2022-03-31 15:35:10 -0600

    update README.md

--- test/network/mainnet_test.dart
+++ test/network/mainnet_test.dart
@@ -4,16 +4,16 @@ import 'package:chia_utils/chia_crypto_utils.dart';
 import 'package:path/path.dart' as path;
 
 Future<void> main() async {
-  final homePath = path.absolute(Platform.environment['HOME']!);
-  print(homePath);
-  final fullNodeRpc  = FullNodeHttpRpc(
-    'https://localhost:8555',
-    certBytes: Bytes(File(path.join(homePath, '.chia/mainnet/config/ssl/full_node/private_full_node.crt')).readAsBytesSync()),
-    keyBytes: Bytes(File(path.join(homePath, '.chia/mainnet/config/ssl/full_node/private_full_node.key')).readAsBytesSync())
-  );
+  // final homePath = path.absolute(Platform.environment['HOME']!);
+  // print(homePath);
+  // final fullNodeRpc  = FullNodeHttpRpc(
+  //   'https://localhost:8555',
+  //   certBytes: Bytes(File(path.join(homePath, '.chia/mainnet/config/ssl/full_node/private_full_node.crt')).readAsBytesSync()),
+  //   keyBytes: Bytes(File(path.join(homePath, '.chia/mainnet/config/ssl/full_node/private_full_node.key')).readAsBytesSync())
+  // );
 
-  final fullNode = ChiaFullNodeInterface(fullNodeRpc);
+  // final fullNode = ChiaFullNodeInterface(fullNodeRpc);
 
-  final coins = await fullNode.getCoinsByPuzzleHashes([Puzzlehash.fromHex('0b7a3d5e723e0b046fd51f95cabf2d3e2616f05d9d1833e8166052b43d9454ad')]);
-  print(coins);
+  // final coins = await fullNode.getCoinsByPuzzleHashes([Puzzlehash.fromHex('0b7a3d5e723e0b046fd51f95cabf2d3e2616f05d9d1833e8166052b43d9454ad')]);
+  // print(coins);
 }
