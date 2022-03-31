0cbbeb869a69b5695d9fad7cc63399c65e3a0f86
nvjoshi2 <nvj1300@gmail.com>
2022-03-31 14:33:48 -0600

    add network tests

---
+++ test/network/mainnet_test.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:chia_utils/chia_crypto_utils.dart';
+import 'package:path/path.dart' as path;
+
+Future<void> main() async {
+  final homePath = path.absolute(Platform.environment['HOME']!);
+  print(homePath);
+  final fullNodeRpc  = FullNodeHttpRpc(
+    'https://localhost:8555',
+    certBytes: Bytes(File(path.join(homePath, '.chia/mainnet/config/ssl/full_node/private_full_node.crt')).readAsBytesSync()),
+    keyBytes: Bytes(File(path.join(homePath, '.chia/mainnet/config/ssl/full_node/private_full_node.key')).readAsBytesSync())
+  );
+
+  final fullNode = ChiaFullNodeInterface(fullNodeRpc);
+
+  final coins = await fullNode.getCoinsByPuzzleHashes([Puzzlehash.fromHex('0b7a3d5e723e0b046fd51f95cabf2d3e2616f05d9d1833e8166052b43d9454ad')]);
+  print(coins);
+}
