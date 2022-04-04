a0f45cc7efa3317a5c2f0f018b448c2f837824b1
Frank Moreno <frankmoreno1993@gmail.com>
2022-04-04 09:38:37 -0500

    feature: WalletVector.fromBytes and WalletVector.toBytes()

--- lib/src/core/models/wallet_set.dart
+++ lib/src/core/models/wallet_set.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'dart:typed_data';
+
 import 'package:chia_utils/chia_crypto_utils.dart';
 import 'package:meta/meta.dart';
 
@@ -53,12 +55,40 @@ class WalletSet {
 }
 
 @immutable
-class WalletVector {
+class WalletVector with ToBytesMixin {
   const WalletVector({
     required this.childPrivateKey,
     required this.childPublicKey,
     required this.puzzlehash,
   });
+
+  factory WalletVector.fromBytes(Uint8List bytes) {
+    var length = bytes[0];
+    var left = 1;
+    var right = left + length;
+
+    final childPrivateKey = PrivateKey.fromBytes(bytes.sublist(left, right));
+
+    length = bytes[right];
+    left = right + 1;
+    right = left + length;
+    final childPublicKey = JacobianPoint.fromBytes(
+      bytes.sublist(left, right),
+      bytes[right] == 1,
+    );
+
+    length = bytes[right + 1];
+    left = right + 2;
+    right = left + length;
+
+    final puzzlehash = Puzzlehash(bytes.sublist(left, right));
+
+    return WalletVector(
+      childPrivateKey: childPrivateKey,
+      childPublicKey: childPublicKey,
+      puzzlehash: puzzlehash,
+    );
+  }
 
   final PrivateKey childPrivateKey;
   final JacobianPoint childPublicKey;
@@ -79,6 +109,23 @@ class WalletVector {
             childPrivateKey == other.childPrivateKey &&
             childPublicKey == other.childPublicKey &&
             puzzlehash == other.puzzlehash;
+  }
+
+  @override
+  Uint8List toBytes() {
+    final childPrivateKeyBytes = childPrivateKey.toBytes();
+    final childPublicKeyBytes = childPublicKey.toBytes();
+    final puzzlehashBytes = puzzlehash.toBytes();
+
+    return Uint8List.fromList([
+      childPrivateKeyBytes.length,
+      ...childPrivateKeyBytes,
+      childPublicKeyBytes.length,
+      ...childPublicKeyBytes,
+      if (childPublicKey.isExtension) 1 else 0,
+      puzzlehashBytes.length,
+      ...puzzlehashBytes,
+    ]);
   }
 }
 
