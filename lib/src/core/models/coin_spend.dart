4f091322ebae547bb7d953c260e0f54ece9d74a0
Marvin Quevedo <marvinquevedo2017@outlook.es>
2022-05-02 13:26:58 -0600

    add unknown

--- lib/src/core/models/coin_spend.dart
+++ lib/src/core/models/coin_spend.dart
@@ -6,7 +6,7 @@ import 'package:chia_utils/src/core/models/serializable.dart';
 import 'package:chia_utils/src/standard/puzzles/p2_delegated_puzzle_or_hidden_puzzle/p2_delegated_puzzle_or_hidden_puzzle.clvm.hex.dart';
 import 'package:hex/hex.dart';
 
-class CoinSpend implements Serializable{
+class CoinSpend implements Serializable {
   CoinPrototype coin;
   Program puzzleReveal;
   Program solution;
@@ -18,40 +18,42 @@ class CoinSpend implements Serializable{
   });
 
   Map<String, dynamic> toJson() => <String, dynamic>{
-      'coin': coin.toJson(),
-      'puzzle_reveal': const HexEncoder().convert(puzzleReveal.serialize()),
-      'solution': const HexEncoder().convert(solution.serialize())
-    };
+        'coin': coin.toJson(),
+        'puzzle_reveal': const HexEncoder().convert(puzzleReveal.serialize()),
+        'solution': const HexEncoder().convert(solution.serialize())
+      };
 
   @override
   Bytes toBytes() {
-    return coin.toBytes() + Bytes(puzzleReveal.serialize()) + Bytes(solution.serialize());
+    return coin.toBytes() +
+        Bytes(puzzleReveal.serialize()) +
+        Bytes(solution.serialize());
   }
 
   factory CoinSpend.fromJson(Map<String, dynamic> json) {
     return CoinSpend(
-      coin: CoinPrototype.fromJson(json['coin'] as Map<String, dynamic>) ,
-      puzzleReveal: Program.deserializeHex(json['puzzle_reveal'] as String) ,
+      coin: CoinPrototype.fromJson(json['coin'] as Map<String, dynamic>),
+      puzzleReveal: Program.deserializeHex(json['puzzle_reveal'] as String),
       solution: Program.deserializeHex(json['solution'] as String),
     );
   }
 
   SpendType get type {
     final uncurriedPuzzleSource = puzzleReveal.uncurry().program.toSource();
-    if (uncurriedPuzzleSource == p2DelegatedPuzzleOrHiddenPuzzleProgram.toSource()) {
+    if (uncurriedPuzzleSource ==
+        p2DelegatedPuzzleOrHiddenPuzzleProgram.toSource()) {
       return SpendType.standard;
     }
     if (uncurriedPuzzleSource == catProgram.toSource()) {
       return SpendType.cat;
     }
-    throw UnimplementedError('Unimplemented spend type');
+    return SpendType.unknown;
+    //throw UnimplementedError('Unimplemented spend type');
   }
+
   @override
-  String toString() => 'CoinSpend(coin: $coin, puzzleReveal: $puzzleReveal, solution: $solution)';
+  String toString() =>
+      'CoinSpend(coin: $coin, puzzleReveal: $puzzleReveal, solution: $solution)';
 }
 
-enum SpendType {
-  standard,
-  cat,
-  nft
-}
+enum SpendType { standard, cat, nft, unknown }
