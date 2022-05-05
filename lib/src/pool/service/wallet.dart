7c30cab3d2de7b8fb7b75f404bf920cd69784f96
nvjoshi2 <nvj1300@gmail.com>
2022-05-05 18:09:03 -0500

    pool wallet service and update to singleton service

--- lib/src/pool/service/wallet.dart
+++ lib/src/pool/service/wallet.dart
@@ -1,5 +1,47 @@
+import 'package:chia_utils/chia_crypto_utils.dart';
 import 'package:chia_utils/src/core/service/base_wallet.dart';
+import 'package:chia_utils/src/singleton/puzzles/singleton_top_layer/singleton_top_layer.clvm.hex.dart';
+import 'package:chia_utils/src/singleton/service/singleton_service.dart';
 
 class PoolWalletService extends BaseWalletService {
+//   def create_waiting_room_inner_puzzle(
+//     target_puzzle_hash: bytes32,
+//     relative_lock_height: uint32,
+//     owner_pubkey: G1Element,
+//     launcher_id: bytes32,
+//     genesis_challenge: bytes32,
+//     delay_time: uint64,
+//     delay_ph: bytes32,
+// ) -> Program:
+//     pool_reward_prefix = bytes32(genesis_challenge[:16] + b"\x00" * 16)
+//     p2_singleton_puzzle_hash: bytes32 = launcher_id_to_p2_puzzle_hash(launcher_id, delay_time, delay_ph)
+//     return POOL_WAITING_ROOM_MOD.curry(
+//         target_puzzle_hash, p2_singleton_puzzle_hash, bytes(owner_pubkey), pool_reward_prefix, relative_lock_height
+//     )
+  Program createWaitingRoomInnerPuzzle({
+    Puzzlehash targetPuzzlehash,
+    int relativeLockHeight,
+    JacobianPoint ownerPublicKey,
+    Bytes launcherId,
+    int delayTime,
+    Puzzlehash delayPuzzlehash,
+  }) {
+    final poolRewardPrefix = Bytes.fromHex(blockchainNetwork.aggSigMeExtraData).sublist(0, 16) +
+        Bytes(List.filled(16, 0));
+    final p2SingletonPuzzlehash = launcherIdToP2Puzzlehash(launcherId, delayTime, delayPuzzlehash);
+  }
 
+// def launcher_id_to_p2_puzzle_hash(launcher_id: bytes32, seconds_delay: uint64, delayed_puzzle_hash: bytes32) -> bytes32:
+//     return create_p2_singleton_puzzle(
+//         SINGLETON_MOD_HASH, launcher_id, int_to_bytes(seconds_delay), delayed_puzzle_hash
+//     ).get_tree_hash()
+  Puzzlehash launcherIdToP2Puzzlehash(
+      Bytes launcherId, int secondsDelay, Puzzlehash delayedPuzzlehash) {
+    return SingletonService.createP2SingletonPuzzle(
+      singletonModHash: singletonTopLayerProgram.hash(),
+      launcherId: launcherId,
+      secondsDelay: secondsDelay,
+      delayedPuzzlehash: delayedPuzzlehash,
+    ).hash();
+  }
 }
--- lib/src/singleton/service/singleton_service.dart
+++ lib/src/singleton/service/singleton_service.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'dart:typed_data';
+
 import 'package:chia_utils/chia_crypto_utils.dart';
 import 'package:chia_utils/src/core/service/base_wallet.dart';
+import 'package:chia_utils/src/singleton/puzzles/p2_singleton_or_delayed_puzhash/p2_singleton_or_delayed_puzhash.clvm.hex.dart';
 import 'package:chia_utils/src/singleton/puzzles/singleton_launcher/singleton_launcher.clvm.hex.dart';
 import 'package:chia_utils/src/singleton/puzzles/singleton_top_layer_v1_1/singleton_top_layer_v1_1.clvm.hex.dart';
 
@@ -19,4 +22,19 @@ class SingletonService extends BaseWalletService {
         Program.fromInt(amount),
         Program.fromBytes(List.filled(128, 0)),
       ]);
+
+      static Program createP2SingletonPuzzle({
+        required Bytes singletonModHash,
+        required Bytes launcherId,
+        required int secondsDelay,
+        required Puzzlehash delayedPuzzlehash,
+      }) {
+        return p2SingletonOrDelayedPuzhashProgram.curry([
+          Program.fromBytes(singletonModHash),
+          Program.fromBytes(launcherId),
+          Program.fromBytes(singletonLauncherProgram.hash()),
+          Program.fromBytes(intToBytesStandard(secondsDelay, Endian.big)),
+          Program.fromBytes(delayedPuzzlehash),
+        ]);
+      }
 }
