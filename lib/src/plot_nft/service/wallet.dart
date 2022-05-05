6c823d88388165aba693cbf5875eebb979011ff8
nvjoshi2 <nvj1300@gmail.com>
2022-05-05 16:10:27 -0500

    add puzzles

---
+++ lib/src/plot_nft/service/wallet.dart
@@ -0,0 +1,12 @@
+import 'package:chia_utils/chia_crypto_utils.dart';
+import 'package:chia_utils/src/core/service/base_wallet.dart';
+
+class NftWalletService extends BaseWalletService{
+  SpendBundle createLauncherSpendBundle({
+    List<CoinPrototype> coins, 
+    int fee,
+    
+  }) {
+    
+  }
+}
---
+++ lib/src/pool/service/wallet.dart
@@ -0,0 +1,5 @@
+import 'package:chia_utils/src/core/service/base_wallet.dart';
+
+class PoolWalletService extends BaseWalletService {
+
+}
