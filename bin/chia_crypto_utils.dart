e491dbb50244d4f566940e655c0a46848fb073be
Isaac Sobczak <isaacsobczak@users.noreply.github.com>
2022-06-07 17:38:22 -0500

    Add Generate-ColdWallet command

--- bin/chia_crypto_utils.dart
+++ bin/chia_crypto_utils.dart
@@ -1,19 +1,20 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:bip39/bip39.dart';
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
 import 'package:chia_crypto_utils/src/command/plot_nft/create_new_wallet_with_plotnft.dart';
 import 'package:chia_crypto_utils/src/command/plot_nft/get_farming_status.dart';
+import 'package:chia_crypto_utils/src/command/standard/generate_cold_wallet.dart';
 
 late final ChiaFullNodeInterface fullNode;
 
 void main(List<String> args) {
   final runner = CommandRunner<Future<void>>('ccu', 'Chia Crypto Utils Command Line Tools')
     ..addCommand(CreateWalletWithPlotNFTCommand())
-    ..addCommand(GetFarmingStatusCommand());
+    ..addCommand(GetFarmingStatusCommand())
+    ..addCommand(GenerateColdWalletCommand());
   // Add global options
   runner.argParser
     ..addOption('log-level', defaultsTo: 'none')
@@ -30,20 +31,24 @@ void main(List<String> args) {
     exit(0);
   }
 
-  if (results['full-node-url'] == null) {
-    print('Option full-node-url is mandatory.');
-    print(runner.argParser.usage);
-    exit(126);
-  }
+  if (results.command?.name != 'Generate-ColdWallet') {
+    if (results['full-node-url'] == null) {
+      print('Option full-node-url is mandatory.');
+      print(runner.argParser.usage);
+      exit(126);
+    }
 
-  // Do global setup
-  // Configure environment based on user selections
-  ChiaNetworkContextWrapper().registerNetworkContext(stringToNetwork(results['network'] as String));
-  LoggingContext().setLogLevel(stringToLogLevel(results['log-level'] as String));
-  // construct the Chia full node interface
-  fullNode = ChiaFullNodeInterface.fromURL(
-    results['full-node-url'] as String,
-  );
+    // Do global setup
+    // Configure environment based on user selections
+    ChiaNetworkContextWrapper().registerNetworkContext(
+        stringToNetwork(results['network'] as String));
+    LoggingContext().setLogLevel(
+        stringToLogLevel(results['log-level'] as String));
+    // construct the Chia full node interface
+    fullNode = ChiaFullNodeInterface.fromURL(
+      results['full-node-url'] as String,
+    );
+  }
 
   runner.run(args);
 }
@@ -63,7 +68,10 @@ class CreateWalletWithPlotNFTCommand extends Command<Future<void>> {
 
   @override
   Future<void> run() async {
-    final poolService = _getPoolService(argResults!);
+    final poolService = _getPoolService(
+      argResults!['pool-url'] as String,
+      argResults!['certificate-bytes-path'] as String,
+    );
     final mnemonicPhrase = generateMnemonic(strength: 256);
     final mnemonic = mnemonicPhrase.split(' ');
     print('Mnemonic Phrase: $mnemonic');
@@ -95,20 +103,22 @@ class CreateWalletWithPlotNFTCommand extends Command<Future<void>> {
       }
     }
 
-    await createNewWalletWithPlotNFT(
-      keychainSecret,
-      keychain,
-      poolService,
-      fullNode,
-    );
+    try {
+      await createNewWalletWithPlotNFT(
+        keychainSecret,
+        keychain,
+        poolService,
+        fullNode,
+      );
+    } catch (e) {
+      print(e);
+    }
   }
 }
 
 class GetFarmingStatusCommand extends Command<Future<void>> {
   GetFarmingStatusCommand() {
-    argParser
-      ..addOption('pool-url', defaultsTo: 'https://xch-us-west.flexpool.io')
-      ..addOption('certificate-bytes-path', defaultsTo: 'mozilla-ca/cacert.pem');
+    argParser.addOption('certificate-bytes-path', defaultsTo: 'mozilla-ca/cacert.pem');
   }
 
   @override
@@ -119,7 +129,6 @@ class GetFarmingStatusCommand extends Command<Future<void>> {
 
   @override
   Future<void> run() async {
-    final poolService = _getPoolService(argResults!);
     final mnemonicPhrase = stdin.readLineSync();
 
     if (mnemonicPhrase == null) {
@@ -138,8 +147,11 @@ class GetFarmingStatusCommand extends Command<Future<void>> {
     );
 
     final plotNfts = await fullNode.scroungeForPlotNfts(keychain.puzzlehashes);
-
     for (final plotNft in plotNfts) {
+      final poolService = _getPoolService(
+          plotNft.poolState.poolUrl!,
+          argResults!['certificate-bytes-path'] as String,
+      );
       await getFarmingStatus(
         plotNft,
         keychainSecret,
@@ -151,9 +163,20 @@ class GetFarmingStatusCommand extends Command<Future<void>> {
   }
 }
 
-PoolService _getPoolService(ArgResults argResults) {
-  final poolUrl = argResults['pool-url'] as String;
-  final certificateBytesPath = argResults['certificate-bytes-path'] as String;
+class GenerateColdWalletCommand extends Command<Future<void>> {
+  @override
+  String get description => 'Generate an offline cold wallet';
+
+  @override
+  String get name => 'Generate-ColdWallet';
+
+  @override
+  Future<void> run() async {
+    await generateColdWallet();
+  }
+}
+
+PoolService _getPoolService(String poolUrl, String certificateBytesPath) {
   // clone this for certificate chain: https://github.com/Chia-Network/mozilla-ca.git
   final poolInterface = PoolInterface.fromURLAndCertificate(
     poolUrl,
