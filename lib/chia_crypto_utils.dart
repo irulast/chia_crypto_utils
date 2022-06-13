60cfc70901b8f8912ccfc71be51822e13271e9a8
Nathan Joshi <43415278+nvjoshi2@users.noreply.github.com>
2022-06-13 13:16:53 -0500

    Export singleton folder (#41)
    * initial commit
    
    * relative import fix

--- lib/chia_crypto_utils.dart
+++ lib/chia_crypto_utils.dart
@@ -6,5 +6,6 @@ export 'src/context/index.dart';
 export 'src/core/index.dart';
 export 'src/networks/index.dart';
 export 'src/plot_nft/index.dart';
+export 'src/singleton/index.dart';
 export 'src/standard/index.dart';
 export 'src/utils.dart';
