14e5e193562980353bb8b9c0058ab82c4be131cf
Nathan Joshi <43415278+nvjoshi2@users.noreply.github.com>
2022-06-24 17:34:35 +0200

    initial commit (#43)

---
+++ lib/src/networks/chia/mainnet/mainnet_blockchain_network.dart
@@ -0,0 +1,7 @@
+import 'package:chia_crypto_utils/chia_crypto_utils.dart';
+
+final mainnetBlockchainNetwork = BlockchainNetwork(
+  name: 'mainnet',
+  addressPrefix: 'xch',
+  aggSigMeExtraData: 'ccd5bb71183532bff220ba46c268991a3ff07eb358e8255a65c30a2dce0e5fbb',
+);
--- test/context/network_context_test.dart
+++ test/context/network_context_test.dart
@@ -27,4 +27,31 @@ void main() {
       'ae83525ba8d1dd3f09b277de18ca3e43fc0af20d20c4b3e92ef2a48bd291ccb2',
     );
   });
+
+  test('should switch between environments', () {
+    ChiaNetworkContextWrapper().registerNetworkContext(Network.mainnet);
+
+    var blockchainNetwork = ChiaNetworkContextWrapper().blockchainNetwork;
+
+    expect(blockchainNetwork.name, 'mainnet');
+    expect(blockchainNetwork.addressPrefix, 'xch');
+    expect(
+      blockchainNetwork.aggSigMeExtraData,
+      'ccd5bb71183532bff220ba46c268991a3ff07eb358e8255a65c30a2dce0e5fbb',
+    );
+
+    ChiaNetworkContextWrapper().registerNetworkContext(
+      Network.testnet10,
+      environment: Environment.flutter,
+    );
+
+    blockchainNetwork = ChiaNetworkContextWrapper().blockchainNetwork;
+
+    expect(blockchainNetwork.name, 'testnet10');
+    expect(blockchainNetwork.addressPrefix, 'txch');
+    expect(
+      blockchainNetwork.aggSigMeExtraData,
+      'ae83525ba8d1dd3f09b277de18ca3e43fc0af20d20c4b3e92ef2a48bd291ccb2',
+    );
+  });
 }
