71c65f028264d8f5e937e56cff74851f9c118cb7
nvjoshi2 <nvj1300@gmail.com>
2022-07-06 13:57:21 -0500

    export xch service

--- lib/src/api/index.dart
+++ lib/src/api/index.dart
@@ -2,4 +2,5 @@ export './client.dart';
 export './coin_splitting/service/coin_splitting_service.dart';
 export './full_node/index.dart';
 export './pool/index.dart';
+export './standard/xch_service.dart';
 export './utils/blockchain_utils.dart';
