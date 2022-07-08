95eeb575a1c8e5de192a96f78490b4dbb0eae0cf
nvjoshi2 <nvj1300@gmail.com>
2022-07-08 08:10:37 -0500

    clean up xch scan

--- lib/src/api/chia_price/chia_pricer.dart
+++
@@ -1,6 +0,0 @@
-abstract class ChiaPricer {
-  ChiaPricer(this.url);
-  final String url;
-  //TODO: make price response interface implemn\ented by crypto pricers
-  Future<double> getChiaPriceUsd();
-}
--- lib/src/api/chia_price/xch_scan/xch_scan.dart
+++ lib/src/api/chia_price/xch_scan/xch_scan.dart
@@ -1,23 +1,20 @@
 import 'dart:convert';
 
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
-import 'package:chia_crypto_utils/src/api/chia_price/chia_pricer.dart';
 import 'package:chia_crypto_utils/src/api/chia_price/xch_scan/xch_scan_response.dart';
-
-class XchScan implements ChiaPricer {
+// TODO(nvjoshi2): implement chia pricer interface
+class XchScan {
   Client get client => Client(url);
 
-  @override
   String get url => 'https://xchscan.com/api';
 
-  @override
-  Future<double> getChiaPriceUsd() async {
+  Future<XchScanResponse> getChiaPrice() async {
     final response = await client.get(
       Uri.parse('chia-price'),
     );
 
     return XchScanResponse.fromJson(
       jsonDecode(response.body) as Map<String, dynamic>,
-    ).priceUsd;
+    );;
   }
 }
