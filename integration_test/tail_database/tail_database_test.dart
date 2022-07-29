ac40d1a3ca6c6123f0035d06511415b6e624bb43
Nathan Joshi <43415278+nvjoshi2@users.noreply.github.com>
2022-07-29 10:37:50 -0500

    Address prefix getter, add export, fix tail database api and test (#55)
    * initial commit
    
    * add export
    
    * fix tail database test
    
    * remove test logging

--- integration_test/tail_database/tail_database_test.dart
+++ integration_test/tail_database/tail_database_test.dart
@@ -6,15 +6,16 @@ Future<void> main() async {
   final holidayAssetId = Puzzlehash.fromHex(
     '509deafe3cd8bbfbb9ccce1d930e3d7b57b40c964fa33379b18d628175eb7a8f',
   );
-  final chessAssetId = Puzzlehash.fromHex(
-    'a26bb00329235a4b46d0e402f9c1124279dcb1a30c3236679e1a7f1709a8d7c0',
+  final caesarCoinAssetId = Puzzlehash.fromHex(
+    '125ef688c3200d4a82248c31f6dcfe2dc45d549b779c0b3b1ef35568fea840b6',
   );
+
   final tailDatabaseInterface = TailDatabaseApi();
   test('should get tail info for asset ids', () async {
     final holidayTailInfo = await tailDatabaseInterface.getTailInfo(holidayAssetId);
     expect(holidayTailInfo.name, equals('Chia Holiday 2021'));
 
-    final chessTailInfo = await tailDatabaseInterface.getTailInfo(chessAssetId);
-    expect(chessTailInfo.name, equals('Chess'));
+    final chessTailInfo = await tailDatabaseInterface.getTailInfo(caesarCoinAssetId);
+    expect(chessTailInfo.name, equals('A Golden Caesar'));
   });
 }
--- lib/chia_crypto_utils.dart
+++ lib/chia_crypto_utils.dart
@@ -2,6 +2,7 @@ export 'src/api/index.dart';
 export 'src/bls.dart';
 export 'src/cat/index.dart';
 export 'src/clvm.dart';
+export 'src/command/plot_nft/get_farming_status.dart';
 export 'src/context/index.dart';
 export 'src/core/index.dart';
 export 'src/networks/index.dart';
