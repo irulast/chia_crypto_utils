dfc9243c507e952023bec9480456fac53369bea3
Nathan Joshi <43415278+nvjoshi2@users.noreply.github.com>
2022-08-12 09:05:11 -0500

    Reserve Fee Update (#58)

--- lib/src/core/models/conditions/reserve_fee_condition.dart
+++ lib/src/core/models/conditions/reserve_fee_condition.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
+import 'package:chia_crypto_utils/src/standard/exceptions/invalid_condition_cast_exception.dart';
 
 class ReserveFeeCondition implements Condition {
   static int conditionCode = 52;
@@ -9,8 +10,19 @@ class ReserveFeeCondition implements Condition {
 
   ReserveFeeCondition(this.feeAmount);
 
+  factory ReserveFeeCondition.fromProgram(Program program) {
+    final programList = program.toList();
+    if (!isThisCondition(program)) {
+      throw InvalidConditionCastException(ReserveFeeCondition);
+    }
+    return ReserveFeeCondition(programList[1].toInt());
+  }
+
   static bool isThisCondition(Program condition) {
     final conditionParts = condition.toList();
+    if (conditionParts.length != 2) {
+      return false;
+    }
     return conditionParts[0].toInt() == conditionCode;
   }
 
