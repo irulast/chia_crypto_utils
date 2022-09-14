cc3fd3e7b810c6d04f3080ad85d9b1e4a2f7075a
darkverbito <cerm0004@hotmail.com>
2022-09-14 11:54:18 -0500

    split tests

--- integration_test/namesdao/namesdao_test.dart
+++ integration_test/namesdao/namesdao_test.dart
@@ -2,15 +2,19 @@ import 'package:chia_crypto_utils/src/api/namesdao/namesdao_api.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
-  const name1 = '_namesdao.xchh';
-  const name2 = '___CloakedRegistration.xch';
-
   final namesdaoInterface = NamesdaoApi();
   test('should get name info for name', () async {
-    final name1Info = await namesdaoInterface.getNameInfo(name1);
-    expect(name1Info?.address.address, equals(null));
+    const name = '_namesdao.xchh';
+    final nameInfo = await namesdaoInterface.getNameInfo(name);
+    expect(nameInfo?.address.address, equals(null));
+  });
 
-    final name2Info = await namesdaoInterface.getNameInfo(name2);
-    expect(name2Info?.address.address, equals('xch1l9hj8emh7xdk3y2d4kszeuu0z6gn27s9rlc0yz7uqgyjjtd0fegsvgsjtv'));
+  test('should get name info for cloaked registration', () async {
+    const name = '___CloakedRegistration.xch';
+    final nameInfo = await namesdaoInterface.getNameInfo(name);
+    expect(
+      nameInfo?.address.address,
+      equals('xch1l9hj8emh7xdk3y2d4kszeuu0z6gn27s9rlc0yz7uqgyjjtd0fegsvgsjtv'),
+    );
   });
 }
