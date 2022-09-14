95b50563af7f7184d49991513faa53bde9bbdec8
darkverbito <cerm0004@hotmail.com>
2022-09-14 11:45:57 -0500

    formatting, removed dead code

--- lib/src/api/namesdao/models/name_info.dart
+++ lib/src/api/namesdao/models/name_info.dart
@@ -2,21 +2,10 @@ import 'package:chia_crypto_utils/chia_crypto_utils.dart';
 
 class NameInfo {
   NameInfo({
-    //required this.name,
     required this.address,
-    //required this.nftCoinId,
-    //required this.uris,
-    //required this.metaUris,
   });
 
-  //final String name;
   final Address address;
-  //final Bytes nftCoinId;
-  //final List<String> uris;
-  //final List<String> metaUris;
 
-  NameInfo.fromJson(Map<String, dynamic> json)
-      : //name = json['name'] as String,
-        address = Address(json['address'] as String);
-        //nftCoinId = Bytes.fromHex(json['nft_coin_id'] as String),
+  NameInfo.fromJson(Map<String, dynamic> json) : address = Address(json['address'] as String);
 }
