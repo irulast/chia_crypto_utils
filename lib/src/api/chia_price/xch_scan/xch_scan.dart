5a4a2f61cca2088efdd8a034c0d4b62dd1aa63d2
nvjoshi2 <nvj1300@gmail.com>
2022-09-16 18:27:25 -0500

    add abstract xch scan

--- lib/src/api/chia_price/xch_scan/xch_scan.dart
+++ lib/src/api/chia_price/xch_scan/xch_scan.dart
@@ -2,12 +2,19 @@ import 'dart:convert';
 
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
 import 'package:chia_crypto_utils/src/api/chia_price/xch_scan/xch_scan_response.dart';
+
+abstract class XchScan {
+  factory XchScan() => _XchScan();
+  Future<XchScanResponse> getChiaPrice();
+}
+
 // TODO(nvjoshi2): implement chia pricer interface
-class XchScan {
+class _XchScan implements XchScan {
   Client get client => Client(url);
 
   String get url => 'https://xchscan.com/api';
 
+  @override
   Future<XchScanResponse> getChiaPrice() async {
     final response = await client.get(
       Uri.parse('chia-price'),
