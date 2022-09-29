2a6c7d819a79fa4b3a7e24e0da487b5f960e5a82
nvjoshi2 <nvj1300@gmail.com>
2022-09-29 18:13:35 -0500

    initial commit

--- lib/src/core/models/coin_prototype.dart
+++ lib/src/core/models/coin_prototype.dart
@@ -20,7 +20,7 @@ class CoinPrototype with ToBytesMixin {
   CoinPrototype.fromJson(Map<String, dynamic> json)
       : parentCoinInfo = Bytes.fromHex(json['parent_coin_info'] as String),
         puzzlehash = Puzzlehash.fromHex(json['puzzle_hash'] as String),
-        amount = json['amount'] as int;
+        amount = (json['amount'] as num).toInt();
 
   Bytes get id {
     return (parentCoinInfo + puzzlehash + intToBytesStandard(amount, Endian.big)).sha256Hash();
