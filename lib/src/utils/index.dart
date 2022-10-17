ddde7875f60c165e479d6011f6c5f69b059e8f32
nvjoshi2 <nvj1300@gmail.com>
2022-10-17 10:57:59 -0500

    expose isolate utils

--- lib/src/utils/index.dart
+++ lib/src/utils/index.dart
@@ -1,4 +1,5 @@
 export './key_derivation.dart';
 export './select_coins_for_amount.dart';
+export './spawn_and_wait_for_isolate/spawn_and_wait_for_isolate.dart';
 export './split_into_batches.dart';
 export './to_bytes_mixin.dart';
