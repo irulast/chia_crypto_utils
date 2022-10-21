3917daf485030124323c477b2ade790794a69016
nvjoshi2 <nvj1300@gmail.com>
2022-10-21 15:50:47 -0500

    add timeout to full node

--- lib/src/api/full_node/full_node_http_rpc.dart
+++ lib/src/api/full_node/full_node_http_rpc.dart
@@ -10,7 +10,12 @@ import 'package:meta/meta.dart';
 
 @immutable
 class FullNodeHttpRpc implements FullNode {
-  const FullNodeHttpRpc(this.baseURL, {this.certBytes, this.keyBytes});
+  const FullNodeHttpRpc(
+    this.baseURL, {
+    this.certBytes,
+    this.keyBytes,
+    this.timeout = const Duration(seconds: 15),
+  });
 
   factory FullNodeHttpRpc.fromContext() {
     final fullNodeContext = FullNodeContext();
@@ -26,7 +31,14 @@ class FullNodeHttpRpc implements FullNode {
   final Bytes? certBytes;
   final Bytes? keyBytes;
 
-  Client get client => Client(baseURL, certBytes: certBytes, keyBytes: keyBytes);
+  final Duration timeout;
+
+  Client get client => Client(
+        baseURL,
+        certBytes: certBytes,
+        keyBytes: keyBytes,
+        timeout: timeout,
+      );
 
   @override
   Future<CoinRecordsResponse> getCoinRecordsByPuzzleHashes(
