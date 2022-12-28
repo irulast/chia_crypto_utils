754c34771276d06626b05f4098eeebf56d3e0d9c
“meeragjoshi” <112877973+mgjoshi5@users.noreply.github.com>
2022-12-28 12:53:43 -0600

    add function for checking compiled hex

--- test/clsp/compilation_test.dart
+++ test/clsp/compilation_test.dart
@@ -1,17 +1,41 @@
+@Timeout(Duration(minutes: 1))
+
 import 'dart:io';
-import 'dart:convert';
 
 import 'package:test/test.dart';
 
 Future<void> main() async {
-  test('should correctly compile clsp file', () async {
-    var result = await Process.run(
+  Future<bool> checkCompilation(String pathToClsp, String pathToCompiledHex) async {
+    final process = await Process.run(
       'osascript',
       [
         '-e',
-        'do shell script "./test/clsp/compile_clsp.sh test/clsp/genesis_by_coin_id.clsp" with administrator privileges',
+        'do shell script "./test/clsp/compile_clsp.sh $pathToClsp $pathToCompiledHex" with administrator privileges',
       ],
     );
-    stdout.write(result.stdout);
+
+    if (process.exitCode == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  test('should check that compiled hex of genesis_by_coin_id.clsp is correct', () async {
+    final check = await checkCompilation(
+      'lib/src/cat/puzzles/tails/genesis_by_coin_id/genesis_by_coin_id.clsp',
+      'lib/src/cat/puzzles/tails/genesis_by_coin_id/genesis_by_coin_id.clvm.hex',
+    );
+
+    expect(check, isTrue);
+  });
+
+  test('should check that compiled hex of genesis_by_coin_id.clsp is correct', () async {
+    final check = await checkCompilation(
+      'lib/src/cat/puzzles/tails/genesis_by_coin_id/genesis_by_coin_id.clsp',
+      'lib/src/cat/puzzles/tails/meltable_genesis_by_coin_id/meltable_genesis_by_coin_id.clvm.hex',
+    );
+
+    expect(check, isFalse);
   });
 }
--- test/clsp/compile_clsp.sh
+++ test/clsp/compile_clsp.sh
@@ -1,11 +1,31 @@
 #!/usr/local/bin/bash
 
+TEST_DIR="test/clsp/"
+
+cp ./"$1" ./"$TEST_DIR"
+
+BASENAME=$(basename $1)
+
+TEST_FILE="$TEST_DIR$BASENAME"
+
 cd chia-dev-tools
 . ./venv/bin/activate
-pip install chia-dev-tools 
+# pip install chia-dev-tools 
 cd ..
-cdv clsp build $1 -i lib/src/chialisp/include 
+cdv clsp build $TEST_FILE -i lib/src/chialisp/base 
 deactivate
 
+EXTENSION='.hex'
+HEX_FILE="$TEST_FILE$EXTENSION"
+HEX=$(cat $HEX_FILE)
 
+HEX_TO_CHECK=$(cat $2)
+
+rm $TEST_FILE
+rm $HEX_FILE
+
+if [ "$HEX" != "$HEX_TO_CHECK" ]
+then
+  exit 1
+fi
 
