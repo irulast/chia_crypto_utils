3c468a774170323726bdb087dcf14dc80abfbeaa
“meeragjoshi” <112877973+mgjoshi5@users.noreply.github.com>
2023-01-06 16:14:07 -0600

    made more specific ccoffer methods

--- lib/src/exchange/btc/cross_chain_offer/service/cross_chain_offer.dart
+++ lib/src/exchange/btc/cross_chain_offer/service/cross_chain_offer.dart
@@ -1,10 +1,12 @@
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/dexie/dexie.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/dexie/dexie_post_offer_response.dart';
+import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/btc_to_xch_accept_offer_file.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/btc_to_xch_offer_file.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/cross_chain_offer_accept_file.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/cross_chain_offer_file.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/exchange_amount.dart';
+import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/xch_to_btc_accept_offer_file.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/models/xch_to_btc_offer_file.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/cross_chain_offer/utils/cross_chain_offer_file_serialization.dart';
 import 'package:chia_crypto_utils/src/exchange/btc/models/lightning_payment_request.dart';
@@ -16,41 +18,44 @@ class CrossChainOfferService {
   final DexieApi dexieApi = DexieApi();
   final standardWalletService = StandardWalletService();
 
-  CrossChainOfferFile makeCrossChainOfferFile({
+  XchToBtcOfferFile createXchToBtcOfferFile({
     required int amountMojos,
     required int amountSatoshis,
     required Address messageAddress,
     required int validityTime,
     required JacobianPoint requestorPublicKey,
-    LightningPaymentRequest? paymentRequest,
+    required LightningPaymentRequest paymentRequest,
   }) {
-    CrossChainOfferFile? offerFile;
-    if (paymentRequest != null) {
-      final offeredAmount = ExchangeAmount(type: ExchangeAmountType.XCH, amount: amountMojos);
-      final requestedAmount = ExchangeAmount(type: ExchangeAmountType.BTC, amount: amountSatoshis);
+    final offeredAmount = ExchangeAmount(type: ExchangeAmountType.XCH, amount: amountMojos);
+    final requestedAmount = ExchangeAmount(type: ExchangeAmountType.BTC, amount: amountSatoshis);
 
-      offerFile = XchToBtcOfferFile(
-        offeredAmount: offeredAmount,
-        requestedAmount: requestedAmount,
-        messageAddress: messageAddress,
-        validityTime: validityTime,
-        publicKey: requestorPublicKey,
-        lightningPaymentRequest: paymentRequest,
-      );
-    } else {
-      final offeredAmount = ExchangeAmount(type: ExchangeAmountType.BTC, amount: amountSatoshis);
-      final requestedAmount = ExchangeAmount(type: ExchangeAmountType.XCH, amount: amountMojos);
+    return XchToBtcOfferFile(
+      offeredAmount: offeredAmount,
+      requestedAmount: requestedAmount,
+      messageAddress: messageAddress,
+      validityTime: validityTime,
+      publicKey: requestorPublicKey,
+      lightningPaymentRequest: paymentRequest,
+    );
+  }
 
-      offerFile = BtcToXchOfferFile(
-        offeredAmount: offeredAmount,
-        requestedAmount: requestedAmount,
-        messageAddress: messageAddress,
-        validityTime: validityTime,
-        publicKey: requestorPublicKey,
-      );
-    }
+  BtcToXchOfferFile createBtcToXchOfferFile({
+    required int amountMojos,
+    required int amountSatoshis,
+    required Address messageAddress,
+    required int validityTime,
+    required JacobianPoint requestorPublicKey,
+  }) {
+    final offeredAmount = ExchangeAmount(type: ExchangeAmountType.BTC, amount: amountSatoshis);
+    final requestedAmount = ExchangeAmount(type: ExchangeAmountType.XCH, amount: amountMojos);
 
-    return offerFile;
+    return BtcToXchOfferFile(
+      offeredAmount: offeredAmount,
+      requestedAmount: requestedAmount,
+      messageAddress: messageAddress,
+      validityTime: validityTime,
+      publicKey: requestorPublicKey,
+    );
   }
 
   Future<DexiePostOfferResponse> postCrossChainOfferFileToDexie(
@@ -60,6 +65,63 @@ class CrossChainOfferService {
     final serializedOfferFile = serializeCrossChainOfferFile(offerFile, requestorPrivateKey);
     final response = await dexieApi.postOffer(serializedOfferFile);
     return response;
+  }
+
+  XchToBtcOfferAcceptFile createXchToBtcAcceptFile({
+    required String serializedOfferFile,
+    required int validityTime,
+    required JacobianPoint requestorPublicKey,
+    required LightningPaymentRequest paymentRequest,
+  }) {
+    final acceptedOfferHash = Bytes.encodeFromString(serializedOfferFile).sha256Hash();
+
+    return XchToBtcOfferAcceptFile(
+      validityTime: validityTime,
+      publicKey: requestorPublicKey,
+      lightningPaymentRequest: paymentRequest,
+      acceptedOfferHash: acceptedOfferHash,
+    );
+  }
+
+  BtcToXchOfferAcceptFile createBtcToXchAcceptFile({
+    required String serializedOfferFile,
+    required int validityTime,
+    required JacobianPoint requestorPublicKey,
+  }) {
+    final acceptedOfferHash = Bytes.encodeFromString(serializedOfferFile).sha256Hash();
+
+    return BtcToXchOfferAcceptFile(
+      validityTime: validityTime,
+      publicKey: requestorPublicKey,
+      acceptedOfferHash: acceptedOfferHash,
+    );
+  }
+
+  Future<void> sendMessageCoin({
+    required WalletKeychain keychain,
+    required List<Coin> coinsInput,
+    required Puzzlehash messagePuzzlehash,
+    required PrivateKey requestorPrivateKey,
+    required CrossChainOfferAcceptFile offerAcceptFile,
+    Puzzlehash? changePuzzlehash,
+    int fee = 0,
+  }) async {
+    final serializedOfferAcceptFile = serializeCrossChainOfferFile(
+      offerAcceptFile,
+      requestorPrivateKey,
+    );
+
+    final messageSpendBundle = standardWalletService.createSpendBundle(
+      payments: [
+        Payment(50, messagePuzzlehash, memos: <String>[serializedOfferAcceptFile])
+      ],
+      coinsInput: coinsInput,
+      changePuzzlehash: changePuzzlehash,
+      keychain: keychain,
+      fee: fee,
+    );
+
+    await fullNode.pushTransaction(messageSpendBundle);
   }
 
   Future<CrossChainOfferAcceptFile?> getOfferAcceptFileFromMessagePuzzlehash(
