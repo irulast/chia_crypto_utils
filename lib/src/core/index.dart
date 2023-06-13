33dfae25cbf0980029b049aeed5be658316f9b69
Nathan Joshi <43415278+nvjoshi2@users.noreply.github.com>
2023-06-13 11:06:28 -0500

    Add bip 39 check (#117)
    * print invalid words
    
    * exit with message instead of argument error

--- lib/src/core/index.dart
+++ lib/src/core/index.dart
@@ -1,4 +1,5 @@
 export './mixins/index.dart';
 export './models/index.dart';
 export './puzzles/calculate_synthetic_public_key/calculate_synthetic_public_key.clvm.hex.dart';
+export './resources/bip_39_words.dart';
 export './service/base_wallet.dart';
