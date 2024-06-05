c6a785ad6aef095f84b2656639a587a593b2a9d2
darkverbito <darkverbito@users.noreply.github.com>
2024-06-05 12:00:17 -0500

    Update run_tests.sh

--- integration_test/run_tests.sh
+++ integration_test/run_tests.sh
@@ -32,7 +32,7 @@ function test_subdirectories() {
 
           echo "running tests in $name"
 
-          xvfb-run flutter test -d linux ./$dir --coverage --coverage-path=coverage/${name}_lcov.info 
+          flutter test -d linux ./$dir --coverage --coverage-path=coverage/${name}_lcov.info 
 
           stop_simulator
         else
